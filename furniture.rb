9855c0fc65ebfd09a2cba39130c91630eac257d3
Debbie Milburn <debbiemilburn2@gmail.com>
2016-12-14 21:17:38 -0500

    Update furniture class

--- furniture.rb
+++ furniture.rb
@@ -1,11 +1,15 @@
 class Furniture
 
   def initialize(args={})
-    @type = args.fetch(:type, "Miscellaneous")
-    @name = args.fetch(:name, "Unnamed")
-    @assembly_instructions = args.fetch(:assembly_instructions, "None!")
-    @broken = args.fetch(:broken, true)
+    @type = args.fetch("type", "Miscellaneous")
+    @name = args.fetch("name", "Unnamed")
+    @assembly_instructions = args.fetch("assembly_instructions", "None!")
+    @broken = args.fetch("broken", true)
   end
 
+
+  def to_s
+    "#{@name}: #{@type}, #{@assembly_instructions}"
+  end
 end
 
